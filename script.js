95d7280318e375522ebba78987ebb1c209955cbd
imnomee <59981447+imnomee@users.noreply.github.com>
2024-01-01 17:56:47 +0500

    functional

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const insert = document.getElementById('insert');
+
 window.addEventListener('keydown', (e) => {
     console.log(e);
+    const key = e.key;
+    const code = e.code;
+    const keyCode = e.keyCode;
+    console.log(key, code, keyCode);
+    insert.innerHTML = `<div class="key">
+                ${key === ' ' ? code : key}
+                <small>event.key</small>
+            </div>
+            <div class="key">
+                ${keyCode}
+                <small>event.keyCode</small>
+            </div>
+            <div class="key">
+                ${code}
+                <small>event.code</small>
+            </div>`;
 });
